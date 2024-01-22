05d45b05fe434bd07be85b5a41ac0c478b9899d6
Pallavi7597 <66312599+Pallavi7597@users.noreply.github.com>
2024-01-21 18:32:12 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const SECRET_KEY = 'your_secret_key';
+
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://mongodb:27017/SocialDB");
+
+const User = mongoose.model('User', { username: String, email: String, password: String });
+const Post = mongoose.model('Post', { userId: mongoose.Schema.Types.ObjectId, text: String });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: SECRET_KEY, resave: false, saveUninitialized: true, cookie: { secure: false } }));
+
+
+// Insert your authenticateJWT Function code here.
+
+// Insert your requireAuth Function code here.
+
+// Insert your routing HTML code here.
+
+// Insert your user registration code here.
+
+// Insert your user login code here.
+
+// Insert your post creation code here.
+
+// Insert your post updation code here.
+
+// Insert your post deletion code here.
+
+// Insert your user logout code here.
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
